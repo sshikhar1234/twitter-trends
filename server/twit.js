8e8ab9e56cc718fc5a429f27b19d6184e1191ecf
sshikhar1234 <sshikharshah@gmail.com>
2021-04-02 23:16:24 -0400

    Added checkout api git repo

--- server/twit.js
+++ server/twit.js
@@ -3,14 +3,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 var client = new twit({
-  // consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  // consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  // access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  // access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  consumer_key: env.TWITTER_CONSUMER_KEY,
-  consumer_secret: env.TWITTER_CONSUMER_SECRET,
-  access_token: env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: env.TWITTER_ACCESS_TOKEN_SECRET,
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  // consumer_key: env.TWITTER_CONSUMER_KEY,
+  // consumer_secret: env.TWITTER_CONSUMER_SECRET,
+  // access_token: env.TWITTER_ACCESS_TOKEN_KEY,
+  // access_token_secret: env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 module.exports = client;
