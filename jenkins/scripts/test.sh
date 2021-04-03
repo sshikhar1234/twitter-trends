f8903f78e7c1b227c973c0d42e3db83e4aed2750
sshikhar1234 <sshikharshah@gmail.com>
2021-04-03 00:18:21 -0400

    Removed jenkins script dir

--- jenkins/scripts/test.sh
+++
@@ -1,5 +0,0 @@
-echo 'The following "npm" command tests that your simple Node.js/React'
-echo 'application renders satisfactorily. This command actually invokes the test'
-echo 'runner Jest (https://facebook.github.io/jest/).'
-set -x
-npm test
